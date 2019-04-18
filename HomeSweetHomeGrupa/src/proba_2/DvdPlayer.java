24b9d415bba2b49c134706541a2dd299dd0d638a
Jaba033 <jaborskaanna46@gmail.com>
2019-04-18 14:33:29 +0200

    dvd player zmiana

--- HomeSweetHomeGrupa/src/proba_2/DvdPlayer.java
+++ HomeSweetHomeGrupa/src/proba_2/DvdPlayer.java
@@ -1,5 +1,19 @@
 package proba_2;
 
 public class DvdPlayer {
+	
+	public void eject() {
+		
+	}
+	public void Play(String dvd) 
+	{
+		
+	}
+	public void stop() {
+		
+	}
+	public void pause() {
+		
+	}
 
 }
